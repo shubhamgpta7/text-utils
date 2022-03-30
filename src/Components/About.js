7eab9b38745c61a3f90e855d6edc1835fc4fd84e
Shubham <studentshubham38@gmail.com>
2022-03-30 16:54:05 +0545

    1st Updation

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,7 @@ export default function () {
   return (
     <div>
       <p>About.js Works  </p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ex, quas repellat dolores, tempora reiciendis soluta atque, numquam sint rem illum culpa exercitationem quibusdam sequi doloribus saepe voluptatum aliquam odit?</p>
     </div>
   )
 }
