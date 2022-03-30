81a232faa6407a39de5ae2447cf90c353f7f82be
shubhamgpta7 <shubhamgpta7@gmail.com>
2022-03-30 17:18:33 +0545

    2nd update

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,7 @@ export default function () {
     <div>
       <p>About.js Works  </p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ex, quas repellat dolores, tempora reiciendis soluta atque, numquam sint rem illum culpa exercitationem quibusdam sequi doloribus saepe voluptatum aliquam odit?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quo sapiente nulla qui omnis. Animi, commodi cumque ratione quo beatae consectetur ea iusto officia error id veritatis ab doloremque esse!</p>
     </div>
   )
 }
